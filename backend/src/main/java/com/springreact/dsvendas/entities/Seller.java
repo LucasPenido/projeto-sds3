6299d961cba20733defdc10025e5c5a21f94df96
Lucas Penido <lpenidoa@me.com>
2021-05-07 20:14:17 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/springreact/dsvendas/entities/Seller.java
@@ -0,0 +1,27 @@
+package com.springreact.dsvendas.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+}
